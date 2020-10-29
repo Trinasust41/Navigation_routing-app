5bbf1f76811c49e66b0fca78a0235f3fa0517a2c
Trinasust41 <trinasustcse41@gmail.com>
2020-10-29 23:00:49 +0600

    authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:navigationrouting/page2.dart';
-import 'package:navigationrouting/page3.dart';
-import 'package:navigationrouting/page4.dart';
+import 'package:navigationrouting/tasks.dart';
+
+import 'loginScreen.dart';
+
 
 void main() {
-  runApp(Homepage());
+  runApp(MyApp());
 }
 
-class Homepage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        RegistraionScreen.id: (context) => RegistraionScreen(),
-      },
-
-
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.purple,
+      ),
+      home: HomePage(),
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<FirebaseUser>(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          FirebaseUser user = snapshot.data;
+          return TasksPage(uid: user.uid);
+        } else {
+          return LoginScreen();
+        }
+      },
+    );
+  }
+}
